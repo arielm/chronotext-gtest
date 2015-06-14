164b916f1c5d071209b0f18b43dc7df952af6aac
Ariel Malka <ariel@chronotext.org>
2015-06-14 17:00:34 +0300

    BUILDING AND TESTING: USING Ninja BY DEFAULT, WHENEVER POSSIBLE...

--- build-emscripten.sh
+++ build-emscripten.sh
@@ -15,7 +15,8 @@ BUILD_DIR="$SRC_DIR/gtest-build/$INSTALL_PREFIX"
 INSTALL_PATH="$(pwd)/lib/$INSTALL_PREFIX"
 
 emcmake cmake -H"$SRC_DIR/gtest" -B"$BUILD_DIR" \
-  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -G Ninja \
+  -DCMAKE_VERBOSE_MAKEFILE=OFF \
   -DCMAKE_BUILD_TYPE=Release \
   -DLIBRARY_OUTPUT_PATH="$INSTALL_PATH" \
   -Dgtest_disable_pthreads=ON
