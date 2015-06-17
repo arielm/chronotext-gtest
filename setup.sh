07cbe774e7a4bdf0aa8d5a648461ec85c7bfe4af
Ariel Malka <ariel@chronotext.org>
2015-06-18 02:58:27 +0300

    setup.sh: 1) DECLARING $RUN_TEST ENV-VARIABLE. 2) BETTER ERROR-HANDLING

--- setup.sh
+++ setup.sh
@@ -2,5 +2,24 @@
 
 BUILD_DIR="build"
 
+# ---
+
 cmake -H. -B"$BUILD_DIR"
+
+if (( $? )); then
+  echo "CONFIGURATION FAILED!"
+  exit -1
+fi
+
+# ---
+
 cmake --build "$BUILD_DIR"
+
+if (( $? )); then
+  echo "BUILD FAILED!"
+  exit -1
+fi
+
+# ---
+
+export RUN_TEST="ctest -S $(pwd)/cmake/run.cmake -VV"
