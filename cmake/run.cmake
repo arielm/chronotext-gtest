79099e9795af48922066ab29bb8f26738fa3d82e
Ariel Malka <ariel@chronotext.org>
2015-06-19 15:45:01 +0300

    run.cmake: PROPERLY FAILING UPON ERRORS AT EACH STAGE

--- cmake/run.cmake
+++ cmake/run.cmake
@@ -16,9 +16,11 @@ if (DEFINED CLEAN)
   ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 endif()
 
-ctest_start("Experimental") # WILL PROCESS CTestConfig.cmake (WHICH MUST EXIST ALONGSIDE CMakeLists.txt)
+# --- START ---
 
-# ---
+ctest_start(Experimental) # WILL PROCESS CTestConfig.cmake (WHICH MUST EXIST ALONGSIDE CMakeLists.txt)
+
+# --- CONFIGURE ---
 
 set(CTEST_CONFIGURE_COMMAND "${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=${CTEST_CONFIGURATION_TYPE}")
 set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"-G${CTEST_CMAKE_GENERATOR}\"")
@@ -26,6 +28,24 @@ set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} \"${CTEST_SOURCE_DIRECTO
 set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} -DCMAKE_PREFIX_PATH=${PREFIX_PATH} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_CURRENT_LIST_DIR}/${TOOLCHAIN_FILE}")
 set(CTEST_CONFIGURE_COMMAND "${CTEST_CONFIGURE_COMMAND} -DRUN=ON ${CONFIGURE_ARGS}")
 
-ctest_configure()
-ctest_build()
-ctest_test()
+ctest_configure(RETURN_VALUE res)
+
+if (res)
+  message(FATAL_ERROR "CONFIGURE FAILED!")
+endif()
+
+# --- BUILD ---
+
+ctest_build(RETURN_VALUE res)
+
+if (res)
+  message(FATAL_ERROR "BUILD FAILED!")
+endif()
+
+# --- TEST ---
+
+ctest_test(RETURN_VALUE res)
+
+if (res)
+  message(FATAL_ERROR "TEST FAILED!")
+endif()
